b7890c6c125dfcd8930e39d62f3bfd1143b73e6c
sahil34352 <125258915+sahil34352@users.noreply.github.com>
2024-08-15 16:28:10 +0000

    conversions and comparisons

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -25,3 +25,77 @@ console.log(num);
  let string_number=String(some_number)
  console.log(string_number)
  console.log(typeof(string_number))
+
+ // **************  operations ************** //
+ let value=3
+ let negValue=-value
+ console.log(negValue);
+ 
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**3);
+//  console.log(2/3);
+//  console.log(2%3);
+
+// concatinations of strings
+
+let str1="hello"
+let str2=" world"
+let str3=str1+str2
+console.log(str3);
+
+   // associtivity left to right
+
+// console.log("1"+2);     // "1" and 2 both are different datatypes
+// console.log("2"+1);   //  here as well
+// console.log("1"+ 2 +2 );     // here compiler goes from left to right first print 1 then 2 then 2
+// console.log(2 + 2 + "1")   // here compiler takes two numbers from left side and add them
+
+// do not write this type of code cause it only leads to unreadibility 
+// and also difficult to understand
+
+console.log(+true);   // here + operator is applied to the boolean so it will be treated as 1
+// console.log(true+) not valid
+console.log(+"");      // when addition operator is applied to an empty string then it converts it into 0
+
+let num1,num2,num3
+num1=num2=num3=2+2   // readibility decreases
+
+let gameCounter=100
+gameCounter++;      // postfix operator -> increases the value after usuage
+++gameCounter;      //prefix operator  -> increases the value before usuage
+console.log(gameCounter);
+
+// comparison operators
+
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);   // "2" converts into 2 number
+
+// console.log(null>0);        // here null converts into 0
+// console.log(null==0);       // confusing
+// console.log(null>=0);       // here null converts into 0
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// console.log("2"===2)     // === checks value as well as datatype
+
+
+// avoid these kind of comparisons stated above
+
+
+
+
+
+
+
+ 
+
+ 
